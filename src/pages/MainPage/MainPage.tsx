32cb89a18e3ef2b8d99e32037254f981ac328d68
forellko <dragonforce97q@gmail.com>
2024-06-25 03:03:41 +0300

    Create Main Page

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './style.module.css';
+
+function MainPage() {
+  return (
+    <header>
+      <div className={style.header}>
+        <h1>HS-CARD</h1>
+      </div>
+    </header>
+  );
+}
+
+export default MainPage;
