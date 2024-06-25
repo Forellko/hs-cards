0a492bee202160fe3bdc84ad5d769393a8653f8e
forellko <dragonforce97q@gmail.com>
2024-06-25 06:23:30 +0300

    Create Classes

--- src/pages/MainPage/Aside/Class/index.tsx
+++ src/pages/MainPage/Aside/Class/index.tsx
@@ -1,19 +1,49 @@
 import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { useState } from 'react';
+
+type TClass =
+  | 'Все классы'
+  | 'Рыцарь смерти'
+  | 'Охотник на демонов'
+  | 'Друид'
+  | 'Охотник'
+  | 'Маг'
+  | 'Паладин'
+  | 'Жрец'
+  | 'Разбойник';
+
+const classes: TClass[] = [
+  'Все классы',
+  'Рыцарь смерти',
+  'Охотник на демонов',
+  'Друид',
+  'Охотник',
+  'Маг',
+  'Паладин',
+  'Жрец',
+  'Разбойник',
+];
 
 function Class() {
+  const [heroClass, setHerroClass] = useState<TClass>('Все классы');
+
   return (
-    <FormControl fullWidth>
+    <FormControl
+      fullWidth
+      sx={{ maxWidth: '300px', minWidth: '300px' }}
+      size="small"
+    >
       <InputLabel id="demo-simple-select-label">Класс</InputLabel>
       <Select
         labelId="demo-simple-select-label"
         id="demo-simple-select"
-        value={0}
+        value={heroClass}
         label="Класс"
-        onChange={() => {}}
+        onChange={(event) => setHerroClass(event.target.value as TClass)}
       >
-        <MenuItem value={10}>Ten</MenuItem>
-        <MenuItem value={20}>Twenty</MenuItem>
-        <MenuItem value={30}>Thirty</MenuItem>
+        {classes.map((classEl) => (
+          <MenuItem value={classEl}>{classEl}</MenuItem>
+        ))}
       </Select>
     </FormControl>
   );
