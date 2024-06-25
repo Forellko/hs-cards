1cebb0501f5c326118ff0a282c61e700c1008999
forellko <dragonforce97q@gmail.com>
2024-06-25 06:43:46 +0300

    Create CreationType

--- src/pages/MainPage/Aside/CreationType/index.tsx
+++ src/pages/MainPage/Aside/CreationType/index.tsx
@@ -1,19 +1,59 @@
 import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { useState } from 'react';
+
+type TCreationType =
+  | 'Все'
+  | 'Демон'
+  | 'Дракон'
+  | 'Зверь'
+  | 'Механизм'
+  | 'Мурлок'
+  | 'Нага'
+  | 'Нежить'
+  | 'Пират'
+  | 'Свинобраз'
+  | 'Тотем'
+  | 'Элементаль';
+
+const creationTypes: TCreationType[] = [
+  'Все',
+  'Демон',
+  'Дракон',
+  'Зверь',
+  'Механизм',
+  'Мурлок',
+  'Нага',
+  'Нежить',
+  'Пират',
+  'Свинобраз',
+  'Тотем',
+  'Элементаль',
+];
 
 function CreationType() {
+  const [creationType, setCreationType] = useState<TCreationType>('Все');
+
   return (
-    <FormControl fullWidth>
+    <FormControl
+      fullWidth
+      sx={{ maxWidth: '300px', minWidth: '300px' }}
+      size="small"
+    >
       <InputLabel id="demo-simple-select-label">Тип существа</InputLabel>
       <Select
         labelId="demo-simple-select-label"
         id="demo-simple-select"
-        value={0}
+        value={creationType}
         label="Тип существа"
-        onChange={() => {}}
+        onChange={(event) =>
+          setCreationType(event.target.value as TCreationType)
+        }
       >
-        <MenuItem value={10}>Ten</MenuItem>
-        <MenuItem value={20}>Twenty</MenuItem>
-        <MenuItem value={30}>Thirty</MenuItem>
+        {creationTypes.map((creationType) => (
+          <MenuItem key={creationType} value={creationType}>
+            {creationType}
+          </MenuItem>
+        ))}
       </Select>
     </FormControl>
   );
