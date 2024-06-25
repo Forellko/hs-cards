ddfd147752df2620c9e6f804c86bb70ea7d062b2
forellko <dragonforce97q@gmail.com>
2024-06-25 03:41:45 +0300

    Complete header

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import style from './style.module.css';
+import '@fontsource/roboto/300.css';
+import { TextField } from '@mui/material';
+import HSImage from './img/hs.png';
+import SearchIcon from '@mui/icons-material/Search';
 
 function MainPage() {
   return (
     <header>
       <div className={style.header}>
-        <h1>HS-CARD</h1>
+        <h1 className={style.header_h}>HS-CARD</h1>
+        <div className={style.search}>
+          <div className={style.icon}>
+            <SearchIcon />
+          </div>
+          <TextField id="standard-basic" label="Поиск" variant="standard" />
+        </div>
+        <img src={HSImage} alt="hs" />
       </div>
     </header>
   );
