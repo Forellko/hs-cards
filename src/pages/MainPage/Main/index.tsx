2c3fb7732648fcb2e3d43c24d8b080f7ae24a61e
forellko <dragonforce97q@gmail.com>
2024-06-25 06:59:44 +0300

    Create grid

--- src/pages/MainPage/Main/index.tsx
+++ src/pages/MainPage/Main/index.tsx
@@ -1,6 +1,18 @@
 import style from './style.module.css';
+import hscard from './hscard.png';
 function Main() {
-  return <div className={style.main}>Main</div>;
+  return (
+    <div className={style.main}>
+      <div className={style.grid}>
+        <img src={hscard} alt="card" className={style.card} />
+        <img src={hscard} alt="card" className={style.card} />
+        <img src={hscard} alt="card" className={style.card} />
+        <img src={hscard} alt="card" className={style.card} />
+        <img src={hscard} alt="card" className={style.card} />
+        <img src={hscard} alt="card" className={style.card} />
+      </div>
+    </div>
+  );
 }
 
 export default Main;
