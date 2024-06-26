2413cb9b9bd0c9d5f77550040e859e7fe575133f
forellko <dragonforce97q@gmail.com>
2024-06-26 09:04:11 +0300

    Remove modal from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React, { useState } from 'react';
 import './App.css';
 import MainPage from './pages/MainPage/MainPage';
-import InputCardModal from 'modals/InputCardModal';
-import { useAppDispatch } from 'app/hooks';
-import { getAllCardsThunk } from 'features/cards/cardSlice';
 
 function App() {
-  const [modal, setModal] = useState(false);
-  const dispatch = useAppDispatch();
-
-  const handleClose = () => {
-    setModal(false);
-    dispatch(getAllCardsThunk());
-  };
-
   return (
     <div className="App">
       <MainPage />
-      <button onClick={() => setModal(true)}>Open modal</button>
-      <InputCardModal handleClose={() => handleClose()} open={modal} />
     </div>
   );
 }
