9c3cb99eaf2b040b27227996053289d429b725d6
forellko <dragonforce97q@gmail.com>
2024-06-26 10:27:59 +0300

    Create card filter

--- src/pages/MainPage/Main/index.tsx
+++ src/pages/MainPage/Main/index.tsx
@@ -1,6 +1,16 @@
 import style from './style.module.css';
 import { useEffect, useState } from 'react';
-import { getAllCardsThunk, selectCards } from 'features/cards/cardSlice';
+import {
+  getAllCardsThunk,
+  selectAddition,
+  selectCards,
+  selectCardType,
+  selectClassHero,
+  selectCost,
+  selectCreationType,
+  selectRarity,
+  selectSpellsSchool,
+} from 'features/cards/cardSlice';
 import { useAppDispatch, useAppSelector } from 'app/hooks';
 import OpenCardModal from 'modals/OpenCardModal';
 import ICard from 'types/Card';
@@ -9,6 +19,45 @@ function Main() {
   const cards = useAppSelector(selectCards);
   const [openModal, setOpenModal] = useState(false);
   const [currentCard, setCurrentCard] = useState<ICard | null>(null);
+  const [filteredCards, setFilteredCards] = useState<ICard[] | null>(null);
+
+  const additionSelect = useAppSelector(selectAddition);
+  const cardTypeSelect = useAppSelector(selectCardType);
+  const classHero = useAppSelector(selectClassHero);
+  const cost = useAppSelector(selectCost);
+  const creationType = useAppSelector(selectCreationType);
+  const rarity = useAppSelector(selectRarity);
+  const spellsSchool = useAppSelector(selectSpellsSchool);
+
+  useEffect(() => {
+    const filtCards =
+      cards
+        .filter(
+          (card) => card.addition === additionSelect || additionSelect === 'Все'
+        )
+        .filter(
+          (card) => card.cardType === cardTypeSelect || cardTypeSelect === 'Все'
+        )
+        .filter((card) => card.classHero === classHero || classHero === 'Все')
+        .filter((card) => card.cost === cost || cost === 'Все')
+        .filter(
+          (card) => card.creationType === creationType || creationType === 'Все'
+        )
+        .filter((card) => card.rarity === rarity || rarity === 'Все')
+        .filter((card) => card.spellsSchool === spellsSchool) ||
+      spellsSchool === 'Все';
+
+    setFilteredCards(filtCards);
+  }, [
+    cards,
+    additionSelect,
+    cardTypeSelect,
+    classHero,
+    cost,
+    creationType,
+    rarity,
+    spellsSchool,
+  ]);
 
   useEffect(() => {
     dispatch(getAllCardsThunk());
@@ -17,8 +66,8 @@ function Main() {
   return (
     <div className={style.main}>
       <div className={style.grid}>
-        {cards &&
-          cards.map((card) => {
+        {filteredCards &&
+          filteredCards.map((card) => {
             return (
               <img
                 src={card.imageURL}
